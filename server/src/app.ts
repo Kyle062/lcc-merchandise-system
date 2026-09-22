639df727fe3aab43d91ea565b005b315ef54e616
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-09-22 14:25:42 +0900

    setting up server

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import productRoutes from './routes/productRoutes';
+import orderRoutes from './routes/orderRoutes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+export default app;
